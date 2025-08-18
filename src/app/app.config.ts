95f89e625c33eb9eb562b623beec0acc4fddef03
timofey <ydepression83@gmail.com>
2025-08-18 08:32:27 +0700

    feat: started project, implemented base logic

---
+++ src/app/app.config.ts
@@ -0,0 +1,25 @@
+import { provideEventPlugins } from "@taiga-ui/event-plugins";
+import { provideAnimations } from "@angular/platform-browser/animations";
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { appRoutes } from './app.routes';
+import {provideYConfig, YConfig} from "angular-yandex-maps-v3";
+
+const config: YConfig = {
+    apikey: "YMAP_API_KEY"
+}
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+        provideAnimations(),
+        provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(appRoutes),
+      provideYConfig(config),
+        provideEventPlugins()
+    ],
+};
