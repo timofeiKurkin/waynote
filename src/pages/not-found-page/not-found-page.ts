ef9df73f1cd109c5c4bbb5ec6a90474053704879
timofey <ydepression83@gmail.com>
2025-08-20 01:55:26 +0700

    feat: implemented not-found page

--- src/pages/not-found-page/not-found-page.ts
+++ src/pages/not-found-page/not-found-page.ts
@@ -1,9 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TuiButton } from '@taiga-ui/core';
+import { TuiBlockStatusComponent } from '@taiga-ui/layout';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-not-found-page',
-  imports: [CommonModule],
+  imports: [CommonModule, TuiButton, TuiBlockStatusComponent, RouterLink],
   templateUrl: './not-found-page.html',
   styleUrl: './not-found-page.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
