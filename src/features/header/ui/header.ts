330f640ca17952562b1f81425a1cd59437022d86
timofey <ydepression83@gmail.com>
2025-08-20 15:37:23 +0700

    feat: header and main page adaptive

--- src/features/header/ui/header.ts
+++ src/features/header/ui/header.ts
@@ -1,9 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { CommonModule, NgOptimizedImage } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 import { AuthService } from '../../../entities/user/state/auth-service';
-import { TuiButton, TuiIcon, TuiLink } from '@taiga-ui/core';
+import {
+  TuiButton,
+  TuiDataList,
+  TuiDropdown,
+  TuiIcon,
+  TuiLink,
+} from '@taiga-ui/core';
 import { UserService } from '../../../entities/user/api/user-service';
+import { TuiTabBarComponent, TuiTabBarItem } from '@taiga-ui/addon-mobile';
 
 @Component({
   selector: 'app-header',
@@ -14,15 +21,39 @@ import { UserService } from '../../../entities/user/api/user-service';
     TuiLink,
     TuiButton,
     TuiIcon,
+    TuiDropdown,
+    TuiDataList,
+    TuiTabBarComponent,
+    TuiTabBarItem,
   ],
   templateUrl: './header.html',
   styleUrl: './header.less',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Header {
+  items = [
+    {
+      icon: '@tui.house',
+      href: '/',
+      text: 'Главная',
+    },
+    {
+      icon: '@tui.route',
+      href: '/my-routes',
+      text: 'Мои маршруты',
+    },
+    {
+      icon: '@tui.circle-plus',
+      href: '/create-route',
+      text: 'Создать маршрут',
+    },
+  ];
+  protected open = false;
+
   constructor(
     private authService: AuthService,
-    private userService: UserService
+    private userService: UserService,
+    private router: Router
   ) {}
 
   get user() {
